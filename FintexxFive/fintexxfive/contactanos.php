5389fcb8a42fb28cfabe8c881824981a801594f5
AleXsanchez27 <99852421+AleXsanchez27@users.noreply.github.com>
2023-06-20 22:54:22 -0700

    Mejoras
    mejoras con muchas paginas mas

---
+++ FintexxFive/fintexxfive/contactanos.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FintexFive</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="js/jquery-3.6.1.js"></script>
+</head>
+<nav class="navbar navbar-expand-lg navbar-dark bg-success">
+        <a class="navbar-brand" href="Index.php">FintexFive</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+<div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="jumbotron">
+                <h1 class="display-4">FintexFive</h1>
+                    <p class="lead">CONTACTANOS
+CORREO: Fintexfve@gmail.com
+<br>
+Telefono: 55-67594-393
+<br>
+fntex es una empresa de venta y union de bienes raices que lo que constituye en el fnanciamiento de inversionistas para
+proyectos de constructuras conocidas creando un modelo de negocios unico e innovador agarrando a la constructora que tenga
+mejor prestigio, y hablando en pocas palabras lo que hacemos es fnanciar proyectos para luego cobrar una comision y regresar
+la inversion con un porcentaje de ganancia, donde nos encargamos de unir al modelo planeado y a la constructura solicitada
+para llevarlo acabo.</p>
+<img src="Imagenes/Suscripcion.jpg" class="d-block w-100" alt="...">
+<a href="index.php" class="btn btn-danger">Volver al Inicio</a>
+                    </div>
+                    <footer class="text-center">
+        <hr>
+        2023 &copy; FintexFive
+    </footer>
+    <script src="js/bootstrap.js"></script>
+
+    </html>
